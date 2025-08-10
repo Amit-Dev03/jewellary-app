268b8bca7a3f4c5f4152a0d7386832d8230bd829
Amit-Dev03 <pandeyamit2426@gmail.com>
2025-08-10 21:59:27 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import "./App.css";
 import Header from "./components/Header";
-import Body from "./components/Body/Body";
 import { Outlet } from "react-router-dom";
 import { Provider } from "react-redux";
 import appStore from "../utils/appStore";
